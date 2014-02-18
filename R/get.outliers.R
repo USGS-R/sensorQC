6da9b4ebfc78fb81dc8088a9835caadbf7586855
Jordan S Read <jread@usgs.gov>
2014-02-18 08:11:59 -0600

    pulled out detect methods to private functions

--- R/get.outliers.R
+++ R/get.outliers.R
@@ -2,6 +2,7 @@ get.outliers	<- 	function(data.in,method='median.absolute.deviation',reject.crit
 	# returns boolean array, equal in size to data.in, with outlier=T or F
 	# NAs are treated as outliers
 	
+	
 	# what is the underlying distribution? (important for assigning "b")
 	
 	#---- <test input data> ----
@@ -13,24 +14,29 @@ get.outliers	<- 	function(data.in,method='median.absolute.deviation',reject.crit
 	
 	#---- <call outlier detection> ----
 	if (method=='median.absolute.deviation'){
-		# from Huber 1981:
-		med.val	<-	median(data.in)					# median of the input data
-		abs.med.diff	<-	abs(data.in-med.val)	# absolute values minus med
-		abs.med	<-	median(abs.med.diff)			# median of these values
-
-		b = 1.4826		# assuming a normal distribution
-		MAD	<-	b*abs.med
-		
-		reject.high	<-	med.val+reject.criteria*MAD
-		reject.low	<-	med.val-reject.criteria*MAD
-		outlier.indices	<-	(data.in>reject.high | data.in<reject.low)
-		
+		outlier.indices	<-	outliersMAD(data.in,reject.criteria=reject.criteria)
 		
 	} else {
 		stop(paste('method',method,'not supported by this function',sep=' '))
 	}
+	#---- </call outlier detection> ----
+	
+	
+	return(outlier.indices)
+}
+
+outliersMAD	<-	function(data.in,reject.criteria){
+	b = 1.4826		# assuming a normal distribution
+	# from Huber 1981:
+	med.val	<-	median(data.in)					# median of the input data
+	abs.med.diff	<-	abs(data.in-med.val)	# absolute values minus med
+	abs.med	<-	median(abs.med.diff)			# median of these values
 
 	
+	MAD	<-	b*abs.med
 	
+	reject.high	<-	med.val+reject.criteria*MAD
+	reject.low	<-	med.val-reject.criteria*MAD
+	outlier.indices	<-	(data.in>reject.high | data.in<reject.low)
 	return(outlier.indices)
 }
