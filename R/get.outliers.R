acb2a818933c62cfd4d4f94a8fbc89a14228a03b
Jordan S Read <jread@usgs.gov>
2014-02-18 07:50:08 -0600

    fleshing out get.outliers function

--- R/get.outliers.R
+++ R/get.outliers.R
@@ -1,5 +1,26 @@
 get.outliers	<- 	function(data.in,method='median.absolute.deviation'){
 	# returns boolean array, equal in size to data.in, with outlier=T or F
+	# NAs are treated as outliers
+	
+	# what is the underlying distribution?
+	
+	#---- <test input data> ----
+	if (!is.numeric(data.in)){
+		stop("input data must be of type 'numeric'")
+	}
+	#---- </test input data> ----
+	
+	
+	#---- <call outlier detection> ----
+	if (method=='median.absolute.deviation'){
+		# from Huber 1981:
+		b = 1.4826
+		MAD	<-	b*median(data.in)*()
+	} else {
+		stop(paste('method',method,'not supported by this function',sep=' '))
+	}
+
+	
 	
 	return(outlier.indices)
 }
