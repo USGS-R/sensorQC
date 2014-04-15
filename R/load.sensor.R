20b291bbf90547575f970e191a7cfae2ff9b5828
Jordan S Read <jread@usgs.gov>
2014-04-15 12:32:48 -0500

    adding date conversion

--- R/load.sensor.R
+++ R/load.sensor.R
@@ -7,6 +7,7 @@ read.pellerin <- function(filename='../data/test_data.txt'){
   # tab delimited with 4 header lines
   
   num.head <- 4
+  delim <- '\t'
   c <- file(filename,"r") #
   
   
@@ -17,7 +18,13 @@ read.pellerin <- function(filename='../data/test_data.txt'){
   blank.date <- rep(as.Date('1900-01-01'),(length(fileLines)*60))
   data.out <- data.frame("DateTime"=blank.date,"sensor.obs"=blank.vec)
   
-  
+  cnt = 1
+  for (i in (num.head+1):length(fileLines)){
+    # for each line, first val is dateTime, second is "record"
+    line <- fileLines[i]
+    line.vals <- strsplit(line,split=delim)
+    date.1 <- as.Date(line.vals[[1]][1],"%m/%d/%Y")
+  }
   
   # should we also return metadata?
   return(data.out)
