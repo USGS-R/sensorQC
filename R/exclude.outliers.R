35484cecea12a87e0efff3148258528e60c81eaf
Jordan S Read <jread@usgs.gov>
2014-04-16 09:58:51 -0500

    set and tested to remove. No write log yet

--- R/exclude.outliers.R
+++ R/exclude.outliers.R
@@ -2,10 +2,16 @@ exclude.outliers <- function(data.in,data.flags,exclude.flags,write.log=TRUE){
   
   # takes in data and data flags, gets rid of outliers, writes log file if applicable
   
-  if (nrow(data.in) != length(flags)){
+  if (nrow(data.in) != length(data.flags)){
     stop("number of columns for data and flags must be equal")
   }
   
+  rmv.i <- data.flags %in% exclude.flags 
   
+  data.out <- data.in[!rmv.i, ]
+  
+  if (write.log){
+    # call write log here..
+  }
   return(data.out)
 }
