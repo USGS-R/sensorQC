1b98d04b9e9986a91a73820a2e88dfd5963919c4
Jordan S Read <jread@usgs.gov>
2014-04-16 09:29:52 -0500

    added handling for irregular data error

--- R/exclude.outliers.R
+++ R/exclude.outliers.R
@@ -1,1 +1,11 @@
-exclude.outliers <- function(data.in,)
+exclude.outliers <- function(data.in,flags,write.log=TRUE){
+  
+  # takes in data and data flags, gets rid of outliers, writes log file if applicable
+  
+  if (nrow(data.in) != length(flags)){
+    stop("number of columns for data and flags must be equal")
+  }
+  
+  
+  return(data.out)
+}
