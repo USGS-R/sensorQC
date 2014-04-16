21be84f493ff560866019f254f6dd9e7c5c898e8
Jordan S Read <jread@usgs.gov>
2014-04-16 08:38:13 -0500

    minor text mod

--- R/window.data.R
+++ R/window.data.R
@@ -1,8 +1,9 @@
-window.data <- function(){
+window.data <- function(data.in,window=30){
   
   # breaks up data into time-windowed chunks
   # returns a list of breaks
   
   
   
+  return(windowed.data)
 }
