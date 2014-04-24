db7c1101dc0ea74f55e87368a9d7923a24a5771a
Jordan S Read <jread@usgs.gov>
2014-04-24 09:26:24 -0500

    added CV to block.out

--- R/block.stats.R
+++ R/block.stats.R
@@ -4,9 +4,13 @@ block.stats <- function(windowed.data,data.flags,params){
   
   un.blcks <- unique(clean.data$block.ID)
 
-  block.out <- init.sensor(length.out=length(un.blcks))
+  block.out <- init.sensor(length.out=length(un.blcks),append=c("CV"))
+  
 
   for (i in seq_len(length(un.blcks))){
+    use.i <- which(clean.data$block.ID==un.blcks[i])
+    block.out$DateTime[i] <- mean(clean.data$DateTime[use.i])
+    block.out$sensor.obs[i] <- mean(clean.data$sensor.obs[use.i])
     
   }
 }
