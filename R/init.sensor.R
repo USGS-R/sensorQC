13469652384952fdc0520c1d244d2d06e300172c
Jordan S Read <jread@usgs.gov>
2014-04-24 09:26:08 -0500

    optional arg for additional columns in sensor df

--- R/init.sensor.R
+++ R/init.sensor.R
@@ -1,8 +1,14 @@
-init.sensor <- function(length.out){
+init.sensor <- function(length.out,append=NULL){
   
   blank.date <- rep(as.POSIXct('1900-01-01'),length.out)
   blank.vec <- vector(length=length.out)*NA
   sensor.out <- data.frame("DateTime"=blank.date,"sensor.obs"=blank.vec)
- 
+  
+  for (i in seq_len(length(append))){
+    name.old <- names(sensor.out)
+    sensor.out <- cbind(sensor.out,blank.vec)
+    names(sensor.out) <- c(name.old,append[i])
+  }
+  
   return(sensor.out)
 }
