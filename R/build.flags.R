dad93b1e9d6a6dfb39f9c015995b5932e24b6bf3
Jordan S Read <jread@usgs.gov>
2014-05-15 08:51:46 -0500

    roxygen for exclude and flags

--- R/build.flags.R
+++ R/build.flags.R
@@ -1,4 +1,25 @@
-build.flags <- function(data.in,params){
+#'@title Creates flag vector based on input data
+#'@description 
+#'Creates flag vector with codes and methods according to params list.  \cr
+#'
+#'@usage
+#'data.flags <- build.flags(data.in,params)
+#'
+#'@param \code{data.in} a data.frame with columns for DateTime and sensor.obs
+#'@param \code{sqc} a sqc object with valid processing parameter names and associated values
+#'@return a vector of flags of length equal to number of rows in data.in
+#'@keywords flags
+#'@author
+#'Jordan S. Read
+#'@examples 
+#'dates <- seq(as.POSIXct('1999-01-01'),by=1,length.out=14)
+#'values <- c(runif(12,2,4),NA,NA)
+#'data.in <- data.frame("DateTime"=dates,"sensor.obs"=values)
+#'
+#'build.flags(data.in,params=NULL)
+#'@export
+
+build.flags <- function(data.in,sqc){
   
   # creates flag array based in data.in and parameters
   
--- R/exclude.outliers.R
+++ R/exclude.outliers.R
@@ -1,3 +1,27 @@
+#'@title removes outliers according to flags
+#'@description 
+#'removes outliers according to flags.  \cr
+#'
+#'@usage
+#'data.out <- exclude.outliers(data.in,data.flags,exclude.flags,write.log=TRUE)
+#'
+#'@param \code{data.in} a time series data.frame
+#'@param \code{data.flags} a numeric vector (same length as ncols in data.in) containing data flags
+#'@param \code{exclude.flags} filter for the type of flags to remove vs ignore
+#'@param \code{write.log} boolean to determine whether log file is written
+#'@return A data.frame with DateTime and values
+#'@keywords outliers
+#'@author
+#'Jordan S. Read
+#'@examples 
+#'dates <- seq(as.POSIXct('1999-01-01'),by=1,length.out=12)
+#'values <- runif(12,2,4)
+#'data.in <- data.frame("DateTime"=dates,"sensor.obs"=values)
+#'data.flags <- c(0,0,0,0,4,0,3,0,3,0,0,0)
+#'exclude.flags <- c(1,4) # only exclude type 1 and type 4 flags
+#'
+#'exclude.outliers(data.in,data.flags,exclude.flags,write.log=F)
+#'@export
 exclude.outliers <- function(data.in,data.flags,exclude.flags,write.log=TRUE){
   
   # takes in data and data flags, gets rid of outliers, writes log file if applicable
