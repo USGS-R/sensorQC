6f9e0cbcdb7707ae59ac391f60a4d17f9e439a88
Jordan S Read <jread@usgs.gov>
2014-06-03 14:56:11 -0500

    adding documentation for threshold

--- R/flag.functions.R
+++ R/flag.functions.R
@@ -1,8 +1,16 @@
-
-
+#'@title threshold tests according to expected format
+#'@details a \code{sensorQC} function for creating flags based on threshold exceedence.\cr 
+#'
+#'@param \code{data.in} name of config file (proceeding *.sqc extension).
+#'@param \code{expr} a valid expression as a string
+#'@return a vector of booleans for flags for this test
+#'@keywords threshold
+#'@author
+#'Jordan S. Read
 threshold <- function(data.in,expr='x > 99'){
   test <- parse(text = expr)
   data.list <- list(x=data.in$sensor.obs)
   names(data.list) <- substr(expr,1,1)
-  eval(test, envir=data.list)
+  flags <- eval(test, envir=data.list)
+  return(flags)
 }
