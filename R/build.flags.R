54edb3051582d8c9452693814dafc2e4eb79298e
Jordan S Read <jread@usgs.gov>
2014-06-05 08:41:59 -0500

    call out to flags parent function, clean on as.char

--- R/build.flags.R
+++ R/build.flags.R
@@ -22,35 +22,14 @@
 build.flags <- function(data.in,sqc){
   
   # creates flag array based in data.in and parameters
-  
-  data.flags <- vector(length=nrow(data.in),mode="numeric") # vector of zeros
-  # types of flagged data: NA=1, 999999=2, MAD=3, etc...
-  # need a matching code function. Or summary function which parameterizes this
-  
-  
-  # NA check
-  rplc.val = 1
-  data.flags[is.na(data.in$sensor.obs)] <- rplc.val
-  
-  # loggernet check
-  rplc.val = 2
-  data.flags[data.in$sensor.obs==999999] <- rplc.val
-  
-  # window data
-  windowed.data <- window.data(data.in=data.in,method="auto")
-  
-  # MAD (if applicable) check
-  rplc.val = 3
-  un.blcks <- unique(windowed.data$block.ID)
-  for (i in seq_len(length(un.blcks))){
-    use.i <- which(windowed.data$block.ID==un.blcks[i] & data.flags==0) # ignore values that are already flagged
-    check.dat <- windowed.data$sensor.obs[use.i]
-    mad.flag <- get.outliers(check.dat,method='median.absolute.deviation',reject.criteria=2.5,na.rm=FALSE)
-    data.flags[use.i[mad.flag]] <- rplc.val
+  data.flags <- vector(length=nrow(data.in)) # vector of zeros
+  for (i in 1:nrow(sqc)){
+    flag.type <- as.character(sqc$QAQC_type[i])
+    expression <- as.character(sqc$expression[i])
+    cat(flag.type);cat('\n')
+    flags <- flag.generic(flag.type,data.in,expr=expression)
+    data.flags <- data.flags | flags
   }
-  
-  # call window.data, then get.outliers, etc. *USE params*
-  
   
   return(data.flags)
   
