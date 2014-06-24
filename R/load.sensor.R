90aa9558ef8324b79d2f7a5c3cff3fefeb58df3c
Jordan S Read <jread@usgs.gov>
2014-06-24 16:11:13 -0500

    Adding pedro format method

--- R/load.sensor.R
+++ R/load.sensor.R
@@ -14,15 +14,51 @@
 #'data.out <- load.sensor(filename,format="Pellerin")
 #'print(data.out)
 #'@export
-
 load.sensor <- function(filename='../examples/test_data.txt',format='Pellerin'){
   
   if (format=='Pellerin'){
     data.out <- read.pellerin(filename)
+  } else if (format=='Pedro'){
+    data.out <- read.pedro(filename)
   } else{
     stop(paste('Sensor data format "',format,'" not supported',sep=''))
   }
   
+  return(data.out)
+}
+
+read.pedro <- function(filename){
+  # tab delimited with 4 header lines
+  
+  num.head <- 4
+  t.step <- 1 #seconds, time between samples on the same row
+  delim <- '\t'
+  c <- file(filename,"r") #
+  
+  
+  fileLines <- readLines(c)
+  close(c)
+  cat('num lines:');cat(length(fileLines));cat('\n')
+  data.out <- init.sensor(length.out=length(fileLines)*60)
+  
+  cnt = 1
+  for (i in (num.head+1):length(fileLines)){
+    # for each line, first val is dateTime, second is "record"
+    line <- fileLines[i]
+    line.vals <- strsplit(line,split=delim)
+    date.1 <- as.POSIXct(strptime(line.vals[[1]][1],"%Y-%m-%d %H:%M"))
+    dat.vals <- line.vals[[1]][c(-1,-2)] # only values (no dates or record number)
+    num.dat <- length(dat.vals)
+    data.out$sensor.obs[cnt:(cnt+num.dat-1)] <- as.numeric(dat.vals)
+    data.out$DateTime[cnt:(cnt+num.dat-1)] <- seq(from=date.1,by="secs",length.out=num.dat)
+    
+    cnt=cnt+num.dat    
+  }
+  
+  rmv.i <- is.na(data.out$sensor.obs)
+  data.out <- data.out[!rmv.i, ]
+  
+  # should we also return metadata?
   return(data.out)
 }
 
@@ -50,8 +86,9 @@ read.pellerin <- function(filename){
     num.dat <- length(dat.vals)
     data.out$sensor.obs[cnt:(cnt+num.dat-1)] <- as.numeric(dat.vals)
     data.out$DateTime[cnt:(cnt+num.dat-1)] <- seq(from=date.1,by="secs",length.out=num.dat)
-    
+      
     cnt=cnt+num.dat
+    
   }
   
   rmv.i <- is.na(data.out$sensor.obs)
