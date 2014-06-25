b070f8eb81ea5da618b5907a640caefdb83444b3
Jordan S Read <jread@usgs.gov>
2014-06-25 16:06:20 -0500

    optional arg for main name

--- R/init.sensor.R
+++ R/init.sensor.R
@@ -1,9 +1,9 @@
-init.sensor <- function(length.out,append=NULL){
+init.sensor <- function(length.out,append=NULL,main.name='sensor.obs'){
   
   blank.date <- rep(as.POSIXct('1900-01-01'),length.out)
   blank.vec <- vector(length=length.out)*NA
-  sensor.out <- data.frame("DateTime"=blank.date,"sensor.obs"=blank.vec)
-  
+  sensor.out <- data.frame("X1"=blank.date,"X2"=blank.vec)
+  names(sensor.out) <- c("DateTime", main.name)  
   for (i in seq_len(length(append))){
     name.old <- names(sensor.out)
     sensor.out <- cbind(sensor.out,blank.vec)
