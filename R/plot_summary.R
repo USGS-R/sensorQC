c93e0724923b8e7f235eb1b1e9344b6c14d99737
Jordan home <jread@usgs.gov>
2014-06-25 21:40:50 -0500

    added compare sensor lines

--- R/plot_summary.R
+++ R/plot_summary.R
@@ -1,5 +1,5 @@
 #'@export
-plot_summary <- function(inst.data,inst.flags,block.data,block.flags,new.plot=TRUE){
+plot_summary <- function(inst.data,inst.flags,block.data,block.flags,compare.data){
   panels = c(2,2,2,1,1,1,1,1,1,1,1,1,3)
   
   
@@ -9,18 +9,21 @@ plot_summary <- function(inst.data,inst.flags,block.data,block.flags,new.plot=TR
        xlab="",xlim=xlm)
   
   layout(panels)
-  par(mai=c(0,.25,0, .1),omi=c(0,0,0,0))
+  par(mai=c(0,.5,0, .1),omi=c(0,0,0,0))
   plot(inst.data[, 1],rep(NA,nrow(inst.data)),ylim=c(30,85),
        ylab="SUNA nitrate concentration (micromoles)",
        xlab="")
   points(inst.data[, 1:2],col="green",pch=19,cex=0.4)
   points(block.data[!block.flags, 1:2],lty=1,lwd=4,pch=19,cex=0.8)
+  lines(compare.data[,1:2],lty=6,col="red",lwd=2)
   plot(c(1,NA),c(NA,0),ylim = c(0,ncol(inst.flags)),xlim = xlm,xlab=NA,axes=F)
+  
   #axis(1,at=seq(0, 200, 10),las=1, cex.axis=cex.box, tck=tck,labels=NA,lwd=2)
   for (i in 1:ncol(inst.flags)){
     flags <- inst.flags[, i]
     use.i <- flags[!is.na(flags)]
     points(inst.data[use.i,1],rep(i,length(use.i)),pch=6)
   }
+  
   
 }
