a1a8e17300ea256d938daa5fa5ae68aca4abfdda
Jordan S Read <jread@usgs.gov>
2014-07-07 10:34:17 -0500

    added date.type arg, made call generic

--- R/load.sensor.R
+++ R/load.sensor.R
@@ -5,21 +5,20 @@
 #'
 #'@param filename a string
 #'@param format a string which matches a valid sensor data format
+#'@param date.type a string which represents a valid date format type
 #'@return A data.frame with DateTime and values
 #'@keywords methods
 #'@author
 #'Jordan S. Read
 #'@examples 
 #'filename <- '../examples/test_data.txt'
-#'data.out <- load.sensor(filename,format="Pellerin")
+#'data.out <- load.sensor(filename,format="wide_burst", date.type="%Y-%m-%d %H:%M")
 #'print(data.out)
 #'@export
-load.sensor <- function(filename='../examples/test_data.txt',format='Pellerin'){
+load.sensor <- function(filename='../examples/test_data.txt',format="wide_burst",date.type){
   
-  if (format=='Pellerin'){
-    data.out <- read.pellerin(filename)
-  } else if (format=='Pedro'){
-    data.out <- read.pedro(filename)
+  if (format=="wide_burst"){
+    data.out <- read.wide_burst(filename,date.type)
   } else{
     stop(paste('Sensor data format "',format,'" not supported',sep=''))
   }
@@ -27,7 +26,7 @@ load.sensor <- function(filename='../examples/test_data.txt',format='Pellerin'){
   return(data.out)
 }
 
-read.pedro <- function(filename){
+read.wide_burst <- function(filename,date.type){
   # tab delimited with 4 header lines
   
   num.head <- 4
@@ -46,49 +45,13 @@ read.pedro <- function(filename){
     # for each line, first val is dateTime, second is "record"
     line <- fileLines[i]
     line.vals <- strsplit(line,split=delim)
-    date.1 <- as.POSIXct(strptime(line.vals[[1]][1],"%Y-%m-%d %H:%M"))
+    date.1 <- as.POSIXct(strptime(line.vals[[1]][1],date.type))
     dat.vals <- line.vals[[1]][c(-1,-2)] # only values (no dates or record number)
     num.dat <- length(dat.vals)
     data.out$sensor.obs[cnt:(cnt+num.dat-1)] <- as.numeric(dat.vals)
     data.out$DateTime[cnt:(cnt+num.dat-1)] <- seq(from=date.1,by="secs",length.out=num.dat)
     
     cnt=cnt+num.dat    
-  }
-  
-  rmv.i <- is.na(data.out$sensor.obs)
-  data.out <- data.out[!rmv.i, ]
-  
-  # should we also return metadata?
-  return(data.out)
-}
-
-read.pellerin <- function(filename){
-  # tab delimited with 4 header lines
-  
-  num.head <- 4
-  t.step <- 1 #seconds, time between samples on the same row
-  delim <- '\t'
-  c <- file(filename,"r") #
-  
-  
-  fileLines <- readLines(c)
-  close(c)
-  
-  data.out <- init.sensor(length.out=length(fileLines)*60)
-  
-  cnt = 1
-  for (i in (num.head+1):length(fileLines)){
-    # for each line, first val is dateTime, second is "record"
-    line <- fileLines[i]
-    line.vals <- strsplit(line,split=delim)
-    date.1 <- as.POSIXct(strptime(line.vals[[1]][1],"%m/%d/%Y %H:%M"))
-    dat.vals <- line.vals[[1]][c(-1,-2)] # only values (no dates or record number)
-    num.dat <- length(dat.vals)
-    data.out$sensor.obs[cnt:(cnt+num.dat-1)] <- as.numeric(dat.vals)
-    data.out$DateTime[cnt:(cnt+num.dat-1)] <- seq(from=date.1,by="secs",length.out=num.dat)
-      
-    cnt=cnt+num.dat
-    
   }
   
   rmv.i <- is.na(data.out$sensor.obs)
