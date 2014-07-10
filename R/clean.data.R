b6cbb6fd1e2a9364458e3a689f85ffa97071eae0
Jordan S Read <jread@usgs.gov>
2014-07-10 10:13:10 -0500

    adding doc and optional args

--- R/clean.data.R
+++ R/clean.data.R
@@ -1,5 +1,22 @@
+#'@title cleans sensor data with user-specified routines
+#'@description 
+#'cleans sensor data according to details within the user-specified config (*yml) file  \cr
+#'
+#'@param deploy a string for the *.yml file name
+#'@param folder a string which specifies the folder of the *.yml file
+#'@param plot.diagnostic a boolean for creating a diagnostic plot
+#'@param write.file a boolean for creating a timeseries file output
+#'@return An optional plot and optional file output handle
+#'@keywords methods
+#'@author
+#'Jordan S. Read
+#'@examples 
+#'\dontrun{
+#'clean.data(deploy='pellerin',folder='/[User]/Desktop/Science Projects/sensorQC/',plot.diagnostic=FALSE)
+#'clean.data(deploy='pellerin',folder='/[User]/Desktop/Science Projects/sensorQC/',plot.diagnostic=TRUE,write.file=TRUE)
+#'}
 #'@export
-clean.data <- function(deploy='pellerin',folder='../examples/'){
+clean.data <- function(deploy='pellerin',folder='../examples/',plot.diagnostic=TRUE, write.file= FALSE){
   #is an example wrapper for sensorQC calls 
   
   #
@@ -23,8 +40,19 @@ clean.data <- function(deploy='pellerin',folder='../examples/'){
   old.inst.flags <- build_flags(data.in=windowed.data,sqc=simple.sqc$outlier_removal,verbose=F)
   old.sensor <- block_stats(windowed.data=windowed.data,old.inst.flags)
   
-  plot_summary(inst.data=windowed.data,inst.flags,block.data=sensor.stats,block.flags,
-               compare.data=old.sensor,sqc=cnfg)
+  if (plot.diagnostic){
+    plot_summary(inst.data=windowed.data,inst.flags,block.data=sensor.stats,block.flags,
+                 compare.data=old.sensor,sqc=cnfg)
+  }
+  
+  if (write.file){
+    output = paste0(folder,deploy,"_sqc_out.tsv")
+    flat.block <- flatten_flags(block.flags)
+    write.out <- sensor.stats[!flat.block, ]
+    write.table(write.out,file=output,col.names=TRUE, quote=FALSE, row.names=FALSE, sep="\t")
+  }
+  
+  
 
   
 
