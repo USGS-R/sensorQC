5e2f47a96152a8b4a34e8bacd13d29006e0b3c80
Jordan S Read <jread@usgs.gov>
2015-09-05 06:42:22 -0500

    separation

---
+++ R/sensor-class.R
@@ -0,0 +1,26 @@
+
+sensor <- function(data){
+  UseMethod('sensor')
+}
+
+#' @export
+sensor.data.frame <- function(data){
+  class(data) <- 'sensor'
+  return(data)
+}
+
+#' @export
+sensor.list <- function(x){
+  data <- x$sensor
+  return(sensor(data))
+}
+
+#' @export
+sensor.flagged <- function(x){
+  UseMethod('sensor', list())
+}
+
+#' @export
+sensor.sensor <- function(sensor){
+  sensor
+}
