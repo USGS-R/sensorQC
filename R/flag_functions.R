4baa560d0647c160fe4ab9c9386954672d04f9d7
Jordan S Read <jread@usgs.gov>
2015-09-08 13:38:33 -0500

    do.call not needed

--- R/flag_functions.R
+++ R/flag_functions.R
@@ -4,7 +4,7 @@ calc_flags <- function(x, ...){
 
 #' @export
 calc_flags.sensor <- function(sensor, expr, which.flagged=TRUE){
-  flags  <-  do.call(sqc, list(expr=expr, vals=values(sensor), window=windows(sensor)))
+  flags  <-  sqc(expr=expr, vals=values(sensor), window=windows(sensor))
   
   if (which.flagged)
     return(which(flags))
