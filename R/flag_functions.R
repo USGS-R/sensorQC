ffd744d626c881c30a815eaf12ca932c58b458a5
Jordan S Read <jread@usgs.gov>
2015-09-09 14:49:19 -0500

    simplifying w/ dplyr

--- R/flag_functions.R
+++ R/flag_functions.R
@@ -2,50 +2,18 @@ calc_flags <- function(x, ...){
   UseMethod('calc_flags')
 }
 
+#' @importFrom dplyr mutate_
+#' @importFrom lazyeval as.lazy
 #' @export
 calc_flags.sensor <- function(sensor, expr, which.flagged=TRUE){
-  flags  <-  sqc(expr=expr, vals=values(sensor), windows=windows(sensor))
+
+  flags <- mutate_(sensor$sensor[-1], flags = lazyeval::as.lazy(expr, globalenv()))$flags
   
+  if (!inherits(flags, 'logical'))
+    stop(expr,' failed to generate booleans')
+  #check for class of flags
   if (which.flagged)
     return(which(flags))
   else  
     return(flags)
-}
-
-
-sqc <- function(expr, vals, windows, ...){
-
-  expr = tryCatch({
-    parse(text = expr)
-  }, error = function(e) {
-    stop(paste0('error evaluation expression ',expr))
-  })
-
-  vals = set.args(expr, vals, windows)
-  
-  flags <- eval(expr, envir=vals)
-  
-  return(flags & is.finite(flags) & !is.na(flags))
-}
-
-set.args <- function(expr, vals, windows){
-  val.call <- function(x){
-    do.call(paste0('to.',x), list(vals=vals, windows=windows))
-  }
-  arg.names = expr_var(expr)
-  args = sapply(arg.names, val.call)
-  return(setNames(args, arg.names))
-}
-
-to.n <- function(vals, ...){  
-  tmp <- rle(vals)
-  list('n'=rep(tmp$lengths,times = tmp$lengths))
-}
-
-to.x <- function(vals, ...){
-  list('x'=vals)
-}
-  
-to.w <- function(..., windows){
-  list('w'=windows)
 }
