b2e8a6c9b8c5c1f88f9fe5c15b82265722cd7da6
Jordan S Read <jread@usgs.gov>
2015-10-20 14:01:16 -0500

    fixes to plot

--- R/plot-sensor.R
+++ R/plot-sensor.R
@@ -6,7 +6,7 @@ plot.sensor <- function(x, y=NULL, ...){
     plot(x$sensor$times, x$sensor$x, ylab='sensor', xlab='xlab')
   } else {
     flag.i <- sort(unique(unlist(sapply(flags(x), function(x) x$flag.i))))
-    plot(x$sensor$times[-flag.i], x$sensor$x[-flag.i], ylab='sensor', xlab='xlab')
+    plot(x$sensor$times[-flag.i], x$sensor$x[-flag.i], ylab='sensor', xlab='DateTime')
   }
   
   
